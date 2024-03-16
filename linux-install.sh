daf37d07c8826153b4ae3af8dc54686686c74f56
Adrian Smith <smith.adriane@gmail.com>
2024-03-16 14:24:27 -0700

    more fixes.

--- linux-install.sh
+++ linux-install.sh
@@ -11,6 +11,9 @@ sudo apt install g++ make zlib1g-dev
 
 wget -O liberica.tar.gz https://download.bell-sw.com/vm/22.3.5/bellsoft-liberica-vm-core-openjdk11.0.22+12-22.3.5+1-linux-amd64.tar.gz
 
-export INSTALL_DIR=~/liberica
-tar -C "$INSTALL_DIR" -xzf liberica.tar.gz
+
+tar -xzf liberica.tar.gz
+
+rm liberica.tar.gz
+mv liberica* liberica/
 
