c2abbe40e8b6fa80026df599df1c26c8008858d8
Adrian Smith <smith.adriane@gmail.com>
2024-03-16 14:20:54 -0700

    try alternat installation method on linux.

--- linux-install.sh
+++ linux-install.sh
@@ -3,13 +3,14 @@
 set -e
 set -x
 
-# sudo apt install g++ make zlib1g-dev
+sudo apt install g++ make zlib1g-dev
 
-wget -O liberica.deb https://download.bell-sw.com/vm/22.3.5/bellsoft-liberica-vm-core-openjdk11.0.22+12-22.3.5+1-linux-amd64.deb
+# wget -O liberica.deb https://download.bell-sw.com/vm/22.3.5/bellsoft-liberica-vm-core-openjdk11.0.22+12-22.3.5+1-linux-amd64.deb
 
-sudo apt install ./liberica.deb
+# sudo apt install ./liberica.deb
 
-ls /opt/bellsoft/
-ls /opt/bellsoft/liberica-vm-core-22.3.5-openjdk11/bin
+wget -O liberica.tar.gz https://download.bell-sw.com/vm/22.3.5/bellsoft-liberica-vm-core-openjdk11.0.22+12-22.3.5+1-linux-amd64.tar.gz
 
-echo 'export PATH=$PATH:/opt/bellsoft/liberica-vm-core-22.3.5-openjdk11/bin' >> ~/.bashrc
+export INSTALL_DIR=~/liberica
+tar -C "$INSTALL_DIR" -xzf liberica.tar.gz
+
