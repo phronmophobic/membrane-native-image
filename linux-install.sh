5092944037fde3d19160b0781653ab16f562dfac
Adrian Smith <smith.adriane@gmail.com>
2024-03-16 14:58:45 -0700

    try building with graalvm.

--- linux-install.sh
+++ linux-install.sh
@@ -5,15 +5,18 @@ set -x
 
 sudo apt install g++ make zlib1g-dev
 
-# wget -O liberica.deb https://download.bell-sw.com/vm/22.3.5/bellsoft-liberica-vm-core-openjdk11.0.22+12-22.3.5+1-linux-amd64.deb
+# liberica
+# wget -O liberica.tar.gz https://download.bell-sw.com/vm/23.1.2/bellsoft-liberica-vm-core-openjdk21.0.2+14-23.1.2+1-linux-amd64.tar.gz
+# tar -xzf liberica.tar.gz
 
-# sudo apt install ./liberica.deb
+# rm liberica.tar.gz
+# mv bellsoft* $HOME/graalvm/
 
-wget -O liberica.tar.gz https://download.bell-sw.com/vm/23.1.2/bellsoft-liberica-vm-core-openjdk21.0.2+14-23.1.2+1-linux-amd64.tar.gz
+# graalvm
+wget -O graalvm.tar.gz https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-21.0.2/graalvm-community-jdk-21.0.2_linux-x64_bin.tar.gz
+tar -xzf graalvm.tar.gz
 
-
-tar -xzf liberica.tar.gz
-
-rm liberica.tar.gz
-mv bellsoft* $HOME/liberica/
+rm graalvm.tar.gz
+ls
+mv graalvm* $HOME/graalvm/
 
