d01661c1d63b014fba952d6fe82dadc18be1d28b
Adrian Smith <smith.adriane@gmail.com>
2024-03-16 14:46:55 -0700

    update liberica version.

--- linux-install.sh
+++ linux-install.sh
@@ -9,7 +9,7 @@ sudo apt install g++ make zlib1g-dev
 
 # sudo apt install ./liberica.deb
 
-wget -O liberica.tar.gz https://download.bell-sw.com/vm/22.3.5/bellsoft-liberica-vm-core-openjdk11.0.22+12-22.3.5+1-linux-amd64.tar.gz
+wget -O liberica.tar.gz https://download.bell-sw.com/vm/23.1.2/bellsoft-liberica-vm-core-openjdk21.0.2+14-23.1.2+1-linux-amd64.tar.gz
 
 
 tar -xzf liberica.tar.gz
