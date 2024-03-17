0c7896f37ef3953f9406fdcfde5a62ff8c17e506
Adrian Smith <smith.adriane@gmail.com>
2024-03-16 23:20:17 -0700

    make main print something even if headless.

--- src/com/phronemophobic/membrane/hello_world.clj
+++ src/com/phronemophobic/membrane/hello_world.clj
@@ -6,7 +6,12 @@
   (:gen-class))
 
 (defn -main [& _args]
-  (java2d/run
-    (membrane.component/make-app
-     #'td/todo-app
-     td/todo-state)))
+  (println
+   (ui/bounds
+    ((membrane.component/make-app
+      #'td/todo-app
+      td/todo-state))))
+  #_(java2d/run
+      (membrane.component/make-app
+       #'td/todo-app
+       td/todo-state)))
